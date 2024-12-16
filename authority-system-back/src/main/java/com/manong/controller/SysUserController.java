9059073004a30148c7c0df6f03b37ed435e3fce1
Aivovaurio <1459538260@qq.com>
2024-12-16 21:43:03 +0800

    token阶段完工

--- authority-system-back/src/main/java/com/manong/controller/SysUserController.java
+++ authority-system-back/src/main/java/com/manong/controller/SysUserController.java
@@ -1,18 +1,28 @@
 package com.manong.controller;
 import com.manong.config.redis.RedisService;
+import com.manong.entity.Permission;
+import com.manong.entity.User;
+import com.manong.entity.UserInfo;
 import com.manong.utils.JwtUtils;
+import com.manong.utils.MenuTree;
 import com.manong.utils.Result;
+import com.manong.vo.RouterVo;
 import com.manong.vo.TokenVo;
 import io.jsonwebtoken.Jwts;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.util.ObjectUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
 @RestController
 @RequestMapping("/api/sysUser")
 public class SysUserController {
@@ -61,4 +71,55 @@ public class SysUserController {
         //返回数据
         return Result.ok(tokenVo).message("token生成成功");
     }
+
+    /**
+     * 获取用户信息
+     *
+     * @return
+     */
+    @GetMapping("/getInfo")
+    public Result getInfo() {
+        //从Spring Security上下文获取用户信息
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        //判断authentication对象是否为空
+        if (authentication == null) {
+            return Result.error().message("用户信息查询失败");
+        }
+        //获取用户信息
+        User user = (User) authentication.getPrincipal();
+        //用户权限集合
+        List<Permission> permissionList = user.getPermissionList();
+        //获取角色权限编码字段
+        Object[] roles = permissionList.stream()
+                .filter(Objects::nonNull)
+                .map(Permission::getCode).toArray();
+        //创建用户信息对象
+        UserInfo userInfo = new UserInfo(user.getId(), user.getNickName(), user.getAvatar(), null, roles);
+        //返回数据
+        return Result.ok(userInfo).message("用户信息查询成功");
+    }
+
+    /**
+     * 获取菜单数据
+     *
+     * @return
+     */
+    @GetMapping("/getMenuList")
+    public Result getMenuList() {
+        //从Spring Security上下文获取用户信息
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        //获取用户信息
+        User user = (User) authentication.getPrincipal();
+        //获取相应的权限
+        List<Permission> permissionList = user.getPermissionList();
+        //筛选目录和菜单
+        List<Permission> collect = permissionList.stream()
+                .filter(item -> item != null && item.getType() !=2)
+                .collect(Collectors.toList());
+        //生成路由数据
+        List<RouterVo> routerVoList = MenuTree.makeRouter(collect, 0L);
+        //返回数据
+        return Result.ok(routerVoList).message("菜单数据获取成功");
+    }
+
 }
